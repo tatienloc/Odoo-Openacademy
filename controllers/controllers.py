7b7db2665f0ad2335de89f21777d50819a1cf8ce
tatienloc <107953189+tatienloc@users.noreply.github.com>
2022-07-04 08:45:14 +0700

    add api in file controllers

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,21 +1,38 @@
-# -*- coding: utf-8 -*-
-# from odoo import http
+from odoo import http
 
 
-# class Openacademy(http.Controller):
-#     @http.route('/openacademy/openacademy/', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
+class Openacademy(http.Controller):
+     @http.route('/openacademy' , auth="public" , type="json" )
+     def index(self, **kw):
+# sửa dữ liệu
+         contect = http.request.env["courses"].sudo().search([("id" , "=" , 26)])
+         contect.write({"name" : kw["name"] ,
+                        "email" : kw["email"] ,
+                        "phone" : kw["phone"] ,
+                        "course" : kw["course"]})
+         return kw
 
-#     @http.route('/openacademy/openacademy/objects/', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('openacademy.listing', {
-#             'root': '/openacademy/openacademy',
-#             'objects': http.request.env['openacademy.openacademy'].search([]),
-#         })
 
-#     @http.route('/openacademy/openacademy/objects/<model("openacademy.openacademy"):obj>/', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('openacademy.object', {
-#             'object': obj
-#         })
+
+
+
+
+# tạo dữ liệu
+#         http.request.env['courses'].sudo().create({'name' : kw['name'] , 'email' : kw['email']
+#                                                       , 'phone' :kw['phone'] , 'course' :kw['course']})
+#         return kw
+
+# lấy dữ liệu
+#         contacts = http.request.env['courses'].sudo().search([])
+
+#         contact_list = []
+#        for contact in contacts:
+#           contact_list.append(
+#          {
+#                'name' : contact.name ,
+#               'email' : contact.email ,
+#              'phone' : contact.phone ,
+#             'course' : contact.course ,
+#     })
+#        return contact_list
+
