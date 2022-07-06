3cdc5541fcf8817b38370e4c3b6ddf9e4300161a
tatienloc <107953189+tatienloc@users.noreply.github.com>
2022-07-06 17:52:56 +0700

    them fields quan he

--- models/course_descripstion.py
+++
@@ -1,8 +0,0 @@
-from odoo import api , fields , models
-
-class Description(models.Model):
-    _name ="description"
-    _description = "Course description"
-
-    acourse = fields.Char(string="Courses")
-    des = fields.Text(string="Description")
